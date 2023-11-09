cc28f9c34620ecbe4320cb50d6df1fef588bbb8a
LRFSousa <luisricardofs@gmail.com>
2023-11-09 17:21:31 -0300

    Payment, one-to-one association

--- src/main/java/com/AulaUdemy/cursoJava/config/TestConfig.java
+++ src/main/java/com/AulaUdemy/cursoJava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.AulaUdemy.cursoJava.entities.Category;
 import com.AulaUdemy.cursoJava.entities.Order;
 import com.AulaUdemy.cursoJava.entities.OrderItem;
+import com.AulaUdemy.cursoJava.entities.Payment;
 import com.AulaUdemy.cursoJava.entities.Product;
 import com.AulaUdemy.cursoJava.entities.User;
 import com.AulaUdemy.cursoJava.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
